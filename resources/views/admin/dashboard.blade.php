50bafee311001c61a9ce92c29ca7ee6120819107
Richard Aroutin <r.aroutin@gmail.com>
2020-06-28 13:27:53 -0400

    Dashboard blank view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,25 +2,6 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row ">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<h1>Dashboard</h1>
 
 @endsection
