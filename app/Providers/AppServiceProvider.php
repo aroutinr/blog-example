2a2864362f65896959d014f78b364ab47e5094c2
Richard Aroutin <r.aroutin@gmail.com>
2020-06-28 13:26:24 -0400

    Viewcomposer for section title

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('admin.categories.*', function ($view) {
+            $view->with('title', \App\Category::TITLE);
+        });
+        View::composer('admin.posts.*', function ($view) {
+            $view->with('title', \App\Post::TITLE);
+        });
+        View::composer('admin.users.*', function ($view) {
+            $view->with('title', \App\User::TITLE);
+        });
     }
 }
